79ab9a7498aec9229c80dcfd5e513016ee484297
Spencer Miskoviak <smiskoviak@gmail.com>
2019-09-12 20:47:01 -0700

    Initial Commit
    The basics of server rendered React app with webpack and babel

---
+++ src/server/html/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+/**
+ * Component to render the essential HTML document information, such as scripts,
+ * links, etc. The initial data can be serialized for the client to consume.
+ * The content is where the client-side React app will pickup.
+ *
+ * @param content the initial render of the React app
+ * @param initialData the initial data to stringify and send to the client
+ */
+const HTML = ({ content, initialData }) => (
+  <html>
+    <head>
+      <title>React SSR</title>
+      <script src="/static/main.js" defer></script>
+    </head>
+    <body>
+      <div id="root" dangerouslySetInnerHTML={{ __html: content }} />
+    </body>
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `window.__INITIAL__DATA__=${JSON.stringify(initialData)}`
+      }}
+    />
+  </html>
+);
+
+export default HTML;
