0f4a501c7370dcd750c0f20d995cd6abba436427
Spencer Miskoviak <smiskoviak@gmail.com>
2019-09-12 20:59:28 -0700

    Setup basic font awesome usage

--- basic-font-awesome/src/shared/home/index.js
+++ basic-font-awesome/src/shared/home/index.js
@@ -1,5 +1,11 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 
-const Hello = props => <h1>Hello, {props.name}!</h1>;
+const Hello = props => (
+  <h1>
+    Hello, {props.name}! <FontAwesomeIcon icon={faCoffee} />
+  </h1>
+);
 
 export default Hello;
